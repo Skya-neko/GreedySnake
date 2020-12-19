20267e57f9dd2fa3f8e62ca8e9ca0b51ff8e5839
Toby0510 <31298962+Toby0510@users.noreply.github.com>
2020-12-19 20:58:35 +0800

    StyleChange
    1.新增食物跟蛇的外觀
    2.新增吃到果實音效
    3.新增碰撞牆壁跟自身的音效
    4.修改背景顏色
    5.修改一格方格為20px
    6.修改行進速度為10

--- GreedySnake_ver1.py
+++ GreedySnake_ver1.py
@@ -3,6 +3,7 @@ import time
 import random
  
 pygame.init()
+pygame.mixer.init()
  
 white = (255, 255, 255)
 yellow = (255, 255, 102)
@@ -10,31 +11,53 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+body = (14, 242, 170)
  
 dis_width = 600
 dis_height = 400
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Edureka')
+pygame.display.set_caption('Snake Game')
+
+star = pygame.image.load("./img/star.png")
+star.convert()
+
+snakeHead = pygame.image.load("./img/snake-head.png")
+snakeHead.convert()
  
 clock = pygame.time.Clock()
  
-snake_block = 10
-snake_speed = 15
+snake_block = 20
+snake_speed = 10
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
+
  
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+def our_snake(snake_list, i):
+    angle = 90 * i
+    rotated_image = pygame.transform.rotate(snakeHead, angle)
+    dis.blit(rotated_image, (snake_list[-1][0], snake_list[-1][1]))
+
+    idx = 0
+    for x in snake_list[:-1]:
+        color1 = 180 + idx * 10
+        if color1 >= 255:
+            color1 = 255
+        pygame.draw.rect(dis, (body[0], color1, body[2]), [x[0], x[1], snake_block, snake_block])
+        idx = idx + 1
  
  
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
- 
- 
+
+
+# Get the coordinate of food
+def getCoordinate(dis):
+    return round(random.randrange(0, dis - snake_block) / snake_block) * snake_block
+
+
 def gameLoop():
     game_over = False
     game_close = False
@@ -44,17 +67,18 @@ def gameLoop():
  
     x1_change = 0
     y1_change = 0
+    iRotate = 0
  
     snake_List = []
     Length_of_snake = 1
  
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    foodx = getCoordinate(dis_width)
+    foody = getCoordinate(dis_height)
  
     while not game_over:
  
         while game_close == True:
-            dis.fill(blue)
+            dis.fill(black)
             message("You Lost! Press C-Play Again or Q-Quit", red)
  
             pygame.display.update()
@@ -72,24 +96,31 @@ def gameLoop():
                 game_over = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
+                    iRotate = 1
                     x1_change = -snake_block
                     y1_change = 0
                 elif event.key == pygame.K_RIGHT:
+                    iRotate = 3
                     x1_change = snake_block
                     y1_change = 0
                 elif event.key == pygame.K_UP:
+                    iRotate = 0
                     y1_change = -snake_block
                     x1_change = 0
                 elif event.key == pygame.K_DOWN:
+                    iRotate = 2
                     y1_change = snake_block
                     x1_change = 0
  
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
+            pygame.mixer.music.load("./sound/crash.mp3")
+            pygame.mixer.music.play(1)
+
         x1 += x1_change
         y1 += y1_change
-        dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        dis.fill(black)
+        dis.blit(star,(foodx, foody))
         snake_Head = []
         snake_Head.append(x1)
         snake_Head.append(y1)
@@ -99,16 +130,20 @@ def gameLoop():
  
         for x in snake_List[:-1]:
             if x == snake_Head:
+                pygame.mixer.music.load("./sound/crash.mp3")
+                pygame.mixer.music.play(1)
                 game_close = True
  
-        our_snake(snake_block, snake_List)
+        our_snake(snake_List, iRotate)
  
  
         pygame.display.update()
  
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            pygame.mixer.music.load("./sound/eat.mp3")
+            pygame.mixer.music.play(1)
+            foodx = getCoordinate(dis_width)
+            foody = getCoordinate(dis_height)
             Length_of_snake += 1
  
         clock.tick(snake_speed)
